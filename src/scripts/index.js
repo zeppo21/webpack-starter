8e89b1c04a5acb878d1924575aabafbea29dfe6c
Paul Martin <p.r.martin.sw@gmail.com>
2020-01-08 10:16:05 -0800

    security module stuff

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,11 @@
-import '../styles/index.scss';
+import "../styles/index.scss";
 
-console.log('webpack starterkit');
+console.log("Hello World!");
+
+window.foo = "secret code";
+
+let inputString = "console.log('Hello Paul')";
+eval(inputString);
+
+let alertString = "alert('Hello')";
+eval(alertString);
